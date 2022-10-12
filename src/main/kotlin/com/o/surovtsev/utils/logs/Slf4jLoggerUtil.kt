cf42dee0f33c317264e9f1ab7315ae87fccb97e6
o.surovtsev <o.surovtsev@spd-ukraine.com>
2022-10-12 17:33:15 +0300

    Slf4jLoggerUtil.kt

--- src/main/kotlin/com/o/surovtsev/utils/logs/Slf4jLoggerUtil.kt
+++ src/main/kotlin/com/o/surovtsev/utils/logs/Slf4jLoggerUtil.kt
@@ -1,4 +1,4 @@
-package com.o.surovtsev.utils
+package com.o.surovtsev.utils.logs
 
 fun org.slf4j.Logger.error(function: () -> String) {
     if (isErrorEnabled) this.error(function.invoke())
@@ -14,10 +14,4 @@ fun org.slf4j.Logger.info(function: () -> String) {
 
 fun org.slf4j.Logger.warn(function: () -> String) {
     if (isWarnEnabled) this.warn(function.invoke())
-}
-
-inline fun <reified T: Enum<T>> T.next(): T {
-    val values = enumValues<T>()
-    val nextOrdinal = (ordinal + 1) % values.size
-    return values[nextOrdinal]
 }
